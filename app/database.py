4d4b92c269ba19e6353da6d92df03b0515e67b62
skr42 <sujeetkr9122@gmail.com>
2026-04-04 00:00:49 +0530

    database connection fix

--- app/database.py
+++ app/database.py
@@ -7,7 +7,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+    
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
